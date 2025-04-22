470bf096a414ffcd94fe95c9a1b7671367518d48
ayumisassaki <ayumisassaki@gmail.com>
2025-04-22 18:13:58 -0300

    add page objects transactionPage.js

---
+++ cypress/pages/transactionPage.js
@@ -0,0 +1,33 @@
+class  TransactionPage {
+
+    selectorsList() {
+        const selectors = {
+            newTransactionButton: '.MuiButton-containedInherit',
+            homePage: '.MuiListSubheader-sticky',
+            userlist: '[data-test="user-list-item-uBmeaz5pX"]',
+            amountField: "[name='amount']",
+            noteField: "[placeholder='Add a note']",
+            submitPayButton: "button[data-test='transaction-create-submit-payment']",
+            sucessTransactionAlert: '[data-test="alert-bar-success"]',
+
+            }
+        
+            return selectors 
+        }
+
+        sendMoneySuccess() {
+            cy.get(this.selectorsList().homePage).contains('Public')
+            cy.get(this.selectorsList().newTransactionButton).click()
+            cy.get(this.selectorsList().userlist).click({force: true})
+            cy.get(this.selectorsList().amountField).type('100')
+            cy.get(this.selectorsList().noteField).type('note')
+            cy.get(this.selectorsList().submitPayButton).click()
+            cy.get(this.selectorsList().sucessTransactionAlert).contains('Transaction Submitted')
+
+        }
+
+
+
+}
+
+export default TransactionPage
--- cypress/tests/transactions.spec.ts
+++ cypress/tests/transactions.spec.ts
@@ -1,7 +1,9 @@
 import LoginPage from '../pages/loginPage.js'
 import userData from '../fixtures/userData.json'
-
-    const loginPage = new LoginPage()
+import TransactionPage from '../pages/transactionPage.js'
+   
+const loginPage = new LoginPage()
+const transactionPage =  new TransactionPage()
 
     
 describe("RWA Tests", () => {
@@ -9,11 +11,11 @@ describe("RWA Tests", () => {
   const selectorsList = {
     newTransactionButton: '.MuiButton-containedInherit',
     homePage: '.MuiListSubheader-sticky',
-    userlist: '[data-test="user-list-item-FWzcftzqU"]',
+    userlist: '[data-test="user-list-item-_XblMqbuoP"]',
     amountField: "[name='amount']",
     noteField: "[placeholder='Add a note']",
     submitPayButton: "button[data-test='transaction-create-submit-payment']",
-    sucessTransactionAlert: '[data-test="alert-bar-success"]'
+    sucessTransactionAlert: '[data-test="alert-bar-success"]',
    }
   
     beforeEach(() => {
@@ -26,17 +28,20 @@ describe("RWA Tests", () => {
     describe('send money with sufficient funds', () => {
     
         it('send money success', () => {
-            cy.get(selectorsList.homePage).contains('Public')
-            cy.get(selectorsList.newTransactionButton).click()
-            cy.get(selectorsList.userlist).click()
-            cy. get(selectorsList.amountField).type('100')
-            cy. get(selectorsList.noteField).type('note')
-            cy.get(selectorsList.submitPayButton).click()
-            cy.get(selectorsList.sucessTransactionAlert).contains('Transaction Submitted')
-        
-
-
+        transactionPage.sendMoneySuccess()
         });
+
+    // describe('send money without sufficient funds', () => {
+    
+    //         it('send money fail', () => {
+    //             cy.get(selectorsList.homePage).contains('Public')
+    //             cy.get(selectorsList.newTransactionButton).click()
+    //             cy.get(selectorsList.userlist).click()
+    //             cy. get(selectorsList.amountField).type('100')
+    //             cy. get(selectorsList.noteField).type('note')
+    //             cy.get(selectorsList.submitPayButton).click()
+                
+    //         });
 
     })
 
